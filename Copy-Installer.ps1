04ef14709cf0617029525ba67fa612f5f1abd14d
kunshanWang <richzinnie@gmail.com>
2020-09-10 16:10:38 +0800

    Create Copy-Installer.ps1
    .add test file.

---
+++ Copy-Installer.ps1
@@ -0,0 +1,18 @@
+﻿PARAM([string]$path, [string]$version, [string]$branch)
+
+$user = "svc-ArchiveMgrBuild@prod.quest.corp";
+$pwd = "Password1";
+
+$uString = [string]::Format("{0}:{1}", $user, $pwd);
+
+$installerSrc = [string]::Format("{0}\\ArchiveManagerInstaller.msi", $path);
+$installerDes = [string]::Format("https://artifactory.labs.quest.com/qam-build/builds/{0}/release/AM-{1}/ArchiveManagerInstaller.msi", $branch, $version);
+
+$autoRunSrc = [string]::Format("{0}\\ArchiveManagerAutoRun.exe", $path);
+$autoRunDes = [string]::Format("https://artifactory.labs.quest.com/qam-build/builds/{0}/release/AM-{1}/ArchiveManagerAutoRun.exe", $branch, $version);
+
+Write-Host "Copying installer from $installerSrc to artifactory: $installerDes.";
+cmd /c curl -u $uString -T $installerSrc $installerDes;
+
+Write-Host "Copying installer from $autoRunSrc to artifactory: $autoRunDes.";
+cmd /c curl -u $uString -T $autoRunSrc $autoRunDes;
